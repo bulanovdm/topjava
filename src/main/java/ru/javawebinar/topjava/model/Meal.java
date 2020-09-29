1d68423518576c141681fe1eb17385ea16e0042a
admin@javaops.ru <gitlaB13>
2020-09-29 15:25:07 +0300

    1_0_rename

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,10 +1,8 @@
 package ru.javawebinar.topjava.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
-public class Meal extends AbstractBaseEntity {
+public class Meal {
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -12,11 +10,6 @@ public class Meal extends AbstractBaseEntity {
     private final int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
-    }
-
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -33,26 +26,4 @@ public class Meal extends AbstractBaseEntity {
     public int getCalories() {
         return calories;
     }
-
-    public LocalDate getDate() {
-        return dateTime.toLocalDate();
-    }
-
-    public LocalTime getTime() {
-        return dateTime.toLocalTime();
-    }
-
-    @Override
-    public String toString() {
-        return "Meal{" +
-                "id=" + id +
-                ", dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                '}';
-    }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1443588... rebase from master_gc
