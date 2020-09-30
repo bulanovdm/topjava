33bd30d3887e90b0fc500a6d6053abf97ff17fe4
admin@javaops.ru <gitlaB13>
2020-10-01 00:26:26 +0300

    1_5_logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,11 +1,5 @@
 package ru.javawebinar.topjava.web;
 
-<<<<<<< HEAD
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-=======
->>>>>>> e99beef... 1_3_add_servlet_api
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,25 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
-<<<<<<< HEAD
-    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int userId = Integer.parseInt(request.getParameter("userId"));
-        SecurityUtil.setAuthUserId(userId);
-        response.sendRedirect("meals");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.debug("forward to users");
-        req.getRequestDispatcher("/users.jsp").forward(req, resp);
-    }
-}
-
-
-=======
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,4 +14,3 @@ public class UserServlet extends HttpServlet {
         response.sendRedirect("users.jsp");
     }
 }
->>>>>>> e99beef... 1_3_add_servlet_api
