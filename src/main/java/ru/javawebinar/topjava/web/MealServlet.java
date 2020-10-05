06ff02d9498678de80490e3dfc73cad67416bdff
dmitriy <bulanovdms@gmail.com>
2020-10-06 02:22:41 +0300

    HW1 realisation

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,43 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
+
+public class MealServlet extends HttpServlet {
+    public static final int EXCESS = 2000;
+    private static final Logger log = getLogger(UserServlet.class);
+    static List<Meal> meals = Arrays.asList(
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+    );
+
+    public static final List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.MIN, LocalTime.MAX, EXCESS);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("redirect to meals");
+
+        req.setAttribute("mealsTo", mealsTo);
+        getServletContext().getRequestDispatcher("/meals.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,36 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="ru">
+<style>
+    tr[red] {
+        color: red;
+    }
+
+    tr[green] {
+        color: green;
+    }
+</style>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+
+<table cellspacing="1" border="1">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <c:forEach items="${mealsTo}" var="meal">
+        <tr ${meal.excess? 'red':'green'}>
+            <td>${meal.date} ${meal.time}
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
