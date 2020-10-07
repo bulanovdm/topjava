a70b334c23024e9f8d5d0ec33da43989fefb206c
admin@javaops.ru <gitlaB13>
2020-10-07 23:31:07 +0300

    2_3_app_layers

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -4,20 +4,17 @@ public abstract class AbstractNamedEntity extends AbstractBaseEntity {
 
     protected String name;
 
-    public AbstractNamedEntity() {
-    }
-
     protected AbstractNamedEntity(Integer id, String name) {
         super(id);
         this.name = name;
     }
 
-    public String getName() {
-        return this.name;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     @Override
--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -1,8 +1,5 @@
 package ru.javawebinar.topjava.model;
 
-import org.springframework.util.CollectionUtils;
-
-import java.util.Collection;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.Set;
@@ -11,32 +8,29 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class User extends AbstractNamedEntity {
 
-    private final Set<Role> roles;
     private String email;
+
     private String password;
+
     private boolean enabled = true;
+
     private Date registered = new Date();
+
+    private Set<Role> roles;
+
     private int caloriesPerDay = DEFAULT_CALORIES_PER_DAY;
 
-    public User() {
-    }
-
-    public User(User u) {
-        this(u.getId(), u.getName(), u.getEmail(), u.getPassword(), u.getCaloriesPerDay(), u.isEnabled(), u.getRegistered(), u.getRoles());
-    }
-
     public User(Integer id, String name, String email, String password, Role role, Role... roles) {
-        this(id, name, email, password, DEFAULT_CALORIES_PER_DAY, true, new Date(), EnumSet.of(role, roles));
+        this(id, name, email, password, DEFAULT_CALORIES_PER_DAY, true, EnumSet.of(role, roles));
     }
 
-    public User(Integer id, String name, String email, String password, int caloriesPerDay, boolean enabled, Date registered, Collection<Role> roles) {
+    public User(Integer id, String name, String email, String password, int caloriesPerDay, boolean enabled, Set<Role> roles) {
         super(id, name);
         this.email = email;
         this.password = password;
         this.caloriesPerDay = caloriesPerDay;
         this.enabled = enabled;
-        this.registered = registered;
-        setRoles(roles);
+        this.roles = roles;
     }
 
     public String getEmail() {
@@ -47,12 +41,20 @@ public class User extends AbstractNamedEntity {
         this.email = email;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Date getRegistered() {
         return registered;
     }
 
     public void setRegistered(Date registered) {
         this.registered = registered;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 
     public int getCaloriesPerDay() {
@@ -67,10 +69,6 @@ public class User extends AbstractNamedEntity {
         return enabled;
     }
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
     public Set<Role> getRoles() {
         return roles;
     }
@@ -79,23 +77,15 @@ public class User extends AbstractNamedEntity {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRoles(Collection<Role> roles) {
-        this.roles = CollectionUtils.isEmpty(roles) ? EnumSet.noneOf(Role.class) : EnumSet.copyOf(roles);
-    }
-
     @Override
     public String toString() {
-        return "User{" +
+        return "User (" +
                 "id=" + id +
                 ", email=" + email +
                 ", name=" + name +
                 ", enabled=" + enabled +
                 ", roles=" + roles +
                 ", caloriesPerDay=" + caloriesPerDay +
-                '}';
+                ')';
     }
 }
