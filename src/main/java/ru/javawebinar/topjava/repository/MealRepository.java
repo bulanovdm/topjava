2f755982d634cc2bfbd43c644b1796b4138b4c90
admin@javaops.ru <gitlaB13>
2020-10-07 23:06:46 +0300

    2_2_HW1_optional

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,26 +2,6 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
-<<<<<<< HEAD
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface MealRepository {
-    // null if updated meal do not belong to userId
-    Meal save(Meal meal, int userId);
-
-    // false if meal do not belong to userId
-    boolean delete(int id, int userId);
-
-    // null if meal do not belong to userId
-    Meal get(int id, int userId);
-
-    // ORDERED dateTime desc
-    List<Meal> getAll(int userId);
-
-    // ORDERED dateTime desc
-    List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId);
-=======
 import java.util.Collection;
 
 public interface MealRepository {
@@ -35,5 +15,4 @@ public interface MealRepository {
     Meal get(int id);
 
     Collection<Meal> getAll();
->>>>>>> d7986e9... 2_2_HW1_optional
 }
