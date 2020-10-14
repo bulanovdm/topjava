63f75c7ad274d4bdfb4becbe815396190f4990e4
admin@javaops.ru <gitlaB13>
2020-10-14 18:51:33 +0300

    3_06_HW2_optional_filter

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.util;
 
-<<<<<<< HEAD
 import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 
@@ -9,16 +8,10 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-=======
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
->>>>>>> d972773... 2_1_HW1
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-<<<<<<< HEAD
     // DB doesn't support LocalDate.MIN/MAX
     private static final LocalDateTime MIN_DATE = LocalDateTime.of(1, 1, 1, 0, 0);
     private static final LocalDateTime MAX_DATE = LocalDateTime.of(3000, 1, 1, 0, 0);
@@ -29,28 +22,17 @@ public class DateTimeUtil {
 
     public static LocalDateTime atStartOfNextDayOrMax(LocalDate localDate) {
         return localDate != null ? localDate.plus(1, ChronoUnit.DAYS).atStartOfDay() : MAX_DATE;
-=======
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
->>>>>>> d972773... 2_1_HW1
     }
 
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
-<<<<<<< HEAD
 
-    public static @Nullable
-    LocalDate parseLocalDate(@Nullable String str) {
+    public static @Nullable LocalDate parseLocalDate(@Nullable String str) {
         return StringUtils.isEmpty(str) ? null : LocalDate.parse(str);
     }
 
-    public static @Nullable
-    LocalTime parseLocalTime(@Nullable String str) {
+    public static @Nullable LocalTime parseLocalTime(@Nullable String str) {
         return StringUtils.isEmpty(str) ? null : LocalTime.parse(str);
     }
 }
-=======
-}
-
->>>>>>> d972773... 2_1_HW1
